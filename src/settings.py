39a024544f10e53b375b72999a70d2d6d71b00ae
Ars Vladimirov <grokhael@gmail.com>
2023-08-04 10:23:32 +0600

    settings

--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,7 @@ from dataset_tools.templates import (
 # * Before uploading to instance #
 ##################################
 PROJECT_NAME: str = "Cityscapes"
-PROJECT_NAME_FULL: Optional[str] = "Cityscapes (Fine Annotations)"
+PROJECT_NAME_FULL: Optional[str] = "Cityscapes (5000 Images)"
 
 ##################################
 # * After uploading to instance ##
