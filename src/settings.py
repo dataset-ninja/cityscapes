535b80aee7e339eb66e72a518d657259a0f20b3c
Ars Vladimirov <grokhael@gmail.com>
2023-08-09 15:45:11 +0600

    HIDE_DATASET = Fals

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Cityscapes"
 PROJECT_NAME_FULL: Optional[str] = "Cityscapes (5000 Images)"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -165,6 +166,7 @@ def get_settings():
 
     settings = {
         "project_name": PROJECT_NAME,
+        "hide_dataset": HIDE_DATASET,
         "license": LICENSE,
         "applications": APPLICATIONS,
         "category": CATEGORY,
