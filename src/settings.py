90e0328f7e67d706966e5f176c49243abdfffd09
Ars Vladimirov <grokhael@gmail.com>
2023-08-21 14:20:21 +0600

    hide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Cityscapes"
 PROJECT_NAME_FULL: Optional[str] = "Cityscapes (5000 Images)"
-HIDE_DATASET = False  # set False when 100% sure about repo quality
+HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
